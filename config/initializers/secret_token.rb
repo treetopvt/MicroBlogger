ce31c38c7855a8e64a035c2ce8a27d29890f93dd
Thomas Bull <bull.thomas@gmail.com>
2012-01-22 10:36:28 -0500

    Initial commit of microblogging app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicroBlogger::Application.config.secret_token = '700041def075a2b7616e41487819d96f01f343ba47801ac6c0c15096dbb0ef6f7062c42fbdd46b3868bec3f0f68517e160fe4d7052b112c0e31abb04b7390cde'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MicroBlogger::Application.config.session_store :cookie_store, key: '_MicroBlogger_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MicroBlogger::Application.config.session_store :active_record_store
